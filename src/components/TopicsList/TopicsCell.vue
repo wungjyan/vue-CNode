bb5707c848cd1af529f6f5f81decdfdf72cb0d52
wungjyan <1170017412@qq.com>
2019-01-16 00:52:54 +0800

    主页主题列表组件

---
+++ src/components/TopicsList/TopicsCell.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="cell">
+    <div class="avatar">
+      <img :src="article.author.avatar_url">
+    </div>
+    <div class="title">
+      <div class="line1">
+        <span v-if="article.top" class="put_top">置顶</span>
+        <span v-else-if="article.good" class="put_good">精华</span>
+        <span v-else class="put_tab">{{getTab(article.tab)}}</span>
+        <div class="article_title">{{article.title}}</div>
+      </div>
+      <div class="line2">
+          <span class="read">
+            <span class="count_of_replies">{{article.reply_count}}</span>
+            <span class="count_seperator">/</span>
+            <span class="count_of_visits">{{article.visit_count}}</span>
+          </span>
+          <span class="time">{{formatTime(article.last_reply_at)}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {
+    getTab (tab) {
+      switch (tab) {
+        case 'share':
+          return '分享'
+        case 'ask':
+          return '问答'
+        case 'job':
+          return '招聘'
+        default:
+          return '分享'
+      }
+    }
+  },
+  mounted () {}
+}
+</script>
+<style lang="scss" scoped>
+.cell {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 140px;
+  border-bottom: 1px solid #eee;
+  .avatar {
+    width: 20%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 90px;
+      height: 90px;
+    }
+  }
+  .title {
+    width: 80%;
+    height: 90px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    box-sizing: border-box;
+    color: #333;
+    .line1 {
+      display: flex;
+      flex-wrap: nowrap;
+      white-space: nowrap;
+      word-break: keep-all;
+      .put_top,
+      .put_good {
+        display: block;
+        background: #80bd01;
+        padding: 4px 8px;
+        border-radius: 6px;
+        color: #fff;
+        font-size: 24px;
+      }
+      .put_tab {
+        display: block;
+        background-color: #e5e5e5;
+        color: #999;
+        padding: 4px 8px;
+        border-radius: 6px;
+        font-size: 24px;
+      }
+      .article_title {
+        font-size: 28px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        margin-left: 10px;
+      }
+    }
+    .line2 {
+      display: flex;
+      justify-content: space-between;
+      font-size: 24px;
+      color: #333;
+      padding: 0 20px 0 80px;
+      .count_of_replies{
+        color: #9e78c0;
+      }
+      .count_of_visits{
+        color: #b4b4b4;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/TopicsList/TopicsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="topics-wrap">
+    <c-loading v-if="loading" />
+    <div v-else>
+      <c-cell v-for="(item,index) in topicsList" :key="index" :article="item"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import cLoading from '../Loading/Loading'
+import cCell from './TopicsCell'
+export default {
+  components: {
+    cLoading,
+    cCell
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState(['loading', 'topicsList'])
+  },
+  methods: {},
+  mounted () {}
+}
+</script>
+<style lang="scss" scoped>
+</style>
