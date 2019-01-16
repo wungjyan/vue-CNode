384b9b6037df18276d85f1263cbb0806c50d4004
wungjyan <1170017412@qq.com>
2019-01-16 15:27:35 +0800

    主题详情页面

---
+++ src/views/Topic.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="topic_detail">
+    <c-header/>
+    <div class="content_wrap">
+      <c-loading v-if="showLoading"/>
+      <div v-else class="content">
+        <div class="topic_header">
+          <div class="title">
+            <span v-if="topicDetail.top" class="put_top">置顶</span>
+            <span v-if="topicDetail.good" class="put_good">精华</span>
+          </div>
+          <div class="changes"></div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import cLoading from '../components/Loading/Loading'
+import cHeader from '../components/Header/Header'
+export default {
+  components: {
+    cLoading,
+    cHeader
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState(['showLoading', 'topicDetail'])
+  },
+  methods: {},
+  mounted () {
+    let id = this.$route.params.id
+    this.$store.dispatch('getTopicDetail', id)
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
