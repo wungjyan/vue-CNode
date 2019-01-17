13419f168b17a0f3c0a374f5c5d42fff3b3e6835
wungjyan <1170017412@qq.com>
2019-01-17 17:14:28 +0800

    添加侧边栏&添加登录

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,61 @@
 <template>
-  <div>
+  <div class="home">
+    <span class="iconfont icon-menu" @click="openMenu"></span>
     <c-header/>
     <c-list/>
+    <transition name="side">
+      <side-bar @hideMenu="closeMenu" @openLogin="goLogin" v-show="showSide"/>
+    </transition>
+    <transition name="login">
+      <c-login @hideLogin="closeLogin" v-show="showLogin"/>
+    </transition>
   </div>
 </template>
 
 <script>
 import cHeader from '../components/Header/Header'
 import cList from '../components/TopicsList/TopicsList'
+import sideBar from '../components/SideBar/SideBar'
+import cLogin from '../components/Login/Login'
 import { mapState } from 'vuex'
 export default {
   components: {
     cHeader,
-    cList
+    cList,
+    sideBar,
+    cLogin
   },
   data () {
     return {
-      busy: false
+      busy: false,
+      showSide: false,
+      showLogin: false
     }
   },
   computed: {
-    ...mapState(['tab', 'current'])
+    ...mapState(['tab', 'current', 'user'])
+  },
+  watch: {
+    // 监听用户是否登录以关闭登录框
+    user (n) {
+      if (n.loginname) {
+        this.showLogin = false
+      }
+    }
   },
   methods: {
+    openMenu () {
+      this.showSide = true
+    },
+    closeMenu () {
+      this.showSide = false
+    },
+    goLogin () {
+      this.showLogin = true
+    },
+    closeLogin () {
+      this.showLogin = false
+    }
   },
   mounted () {
     // 首次加载到首页，获取此数据
@@ -31,5 +64,44 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.home{
+  .iconfont{
+    position: fixed;
+    top: 20px;
+    left: 20px;
+    font-size: 50px;
+    font-weight: 700;
+    color: #ffffff;
+    z-index: 20;
+
+  }
+}
+.side-enter,
+.side-leave-to{
+  opacity: 0;
+  transform: translateX(-100%);
+}
+.side-enter-to,
+.side-leave{
+  opacity: 1;
+  transform: translateX(0);
+}
+.side-enter-active,
+.side-leave-active{
+  transition:all 0.5s;
+}
+
+.login-enter,
+.login-leave-to{
+  opacity: 0;
+}
+.login-enter-to,
+.login-leave{
+  opacity: 1;
+}
+.login-enter-active,
+.login-leave-active{
+    transition:all 0.5s;
+}
 
 </style>
